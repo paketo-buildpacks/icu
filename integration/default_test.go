936d81e68018f3766a352bb1e7e5298fe3d407f0
Arjun Sreedharan <asreedharan@pivotal.io>
2020-07-28 12:17:34 -0400

    Integration tests - online and offline (#39)
    * Add integration tests for online buildpack
    
    Signed-off-by: Arjun Sreedharan <asreedharan@vmware.com>
    
    * integration: add offline tests
    
    Signed-off-by: Arjun Sreedharan <asreedharan@vmware.com>

---
+++ integration/default_test.go
@@ -0,0 +1,80 @@
+package integration_test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/paketo-buildpacks/occam"
+	"github.com/sclevine/spec"
+
+	. "github.com/onsi/gomega"
+)
+
+func testDefault(t *testing.T, context spec.G, it spec.S) {
+	var (
+		Expect = NewWithT(t).Expect
+		pack   occam.Pack
+		docker occam.Docker
+	)
+
+	it.Before(func() {
+		pack = occam.NewPack()
+		docker = occam.NewDocker()
+	})
+
+	context("when building a simple app", func() {
+		var (
+			image     occam.Image
+			container occam.Container
+			name      string
+			source    string
+		)
+
+		it.Before(func() {
+			var err error
+			name, err = occam.RandomName()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		it.After(func() {
+			Expect(docker.Container.Remove.Execute(container.ID)).To(Succeed())
+			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
+			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
+			Expect(os.RemoveAll(source)).To(Succeed())
+		})
+
+		it("builds an oci image with icu installed", func() {
+			var err error
+			source, err = occam.Source(filepath.Join("testdata", "default_app"))
+			Expect(err).NotTo(HaveOccurred())
+
+			var logs fmt.Stringer
+			image, logs, err = pack.WithNoColor().Build.
+				WithNoPull().
+				WithBuildpacks(
+					buildpack,
+					buildPlanBuildpack,
+				).
+				Execute(name, source)
+			Expect(err).NotTo(HaveOccurred(), logs.String())
+
+			Expect(logs).To(ContainSubstring(buildpackInfo.Buildpack.Name))
+			container, err = docker.Container.Run.
+				WithCommand(fmt.Sprintf("icuinfo && test -f /layers/%s/icu/lib/libicudata.so && echo 'libicudata.so exists' && sleep infinity",
+					strings.ReplaceAll(buildpackInfo.Buildpack.ID, "/", "_"))).
+				Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			time.Sleep(5 * time.Second)
+			out, err := docker.Container.Logs.Execute(container.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(out.String()).To(ContainSubstring("International Components for Unicode for C/C++"))
+			Expect(out.String()).To(ContainSubstring("libicudata.so exists"))
+		})
+	})
+}
---
+++ integration/init_test.go
@@ -0,0 +1,102 @@
+package integration_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"github.com/paketo-buildpacks/occam"
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+
+	. "github.com/onsi/gomega"
+)
+
+var (
+	buildpack          string
+	buildPlanBuildpack string
+	offlineBuildpack   string
+	buildpackInfo      struct {
+		Buildpack struct {
+			ID   string
+			Name string
+		}
+	}
+	config struct {
+		BuildPlan string `json:"build-plan"`
+	}
+)
+
+func TestIntegration(t *testing.T) {
+	Expect := NewWithT(t).Expect
+
+	root, err := filepath.Abs("./..")
+	Expect(err).ToNot(HaveOccurred())
+
+	file, err := os.Open("../buildpack.toml")
+	Expect(err).NotTo(HaveOccurred())
+
+	_, err = toml.DecodeReader(file, &buildpackInfo)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(file.Close()).To(Succeed())
+
+	file, err = os.Open("../integration.json")
+	Expect(err).NotTo(HaveOccurred())
+
+	Expect(json.NewDecoder(file).Decode(&config)).To(Succeed())
+	Expect(file.Close()).To(Succeed())
+
+	buildpackStore := occam.NewBuildpackStore()
+
+	buildpack, err = Package(root, "1.2.3", false)
+	Expect(err).NotTo(HaveOccurred())
+
+	offlineBuildpack, err = Package(root, "1.2.3", true)
+	Expect(err).NotTo(HaveOccurred())
+
+	buildPlanBuildpack, err = buildpackStore.Get.
+		Execute(config.BuildPlan)
+	Expect(err).NotTo(HaveOccurred())
+
+	SetDefaultEventuallyTimeout(5 * time.Second)
+
+	suite := spec.New("Integration", spec.Report(report.Terminal{}), spec.Parallel())
+	suite("Default", testDefault)
+	suite("Offline", testOffline)
+	suite.Run(t)
+}
+
+func Package(root, version string, cached bool) (string, error) {
+	var cmd *exec.Cmd
+
+	dir, err := filepath.Abs("./..")
+	if err != nil {
+		return "", err
+	}
+
+	bpPath := filepath.Join(root, "artifact")
+	if cached {
+		cmd = exec.Command(filepath.Join(dir, ".bin", "packager"), "--archive", "--version", version, fmt.Sprintf("%s-cached", bpPath))
+	} else {
+		cmd = exec.Command(filepath.Join(dir, ".bin", "packager"), "--archive", "--uncached", "--version", version, bpPath)
+	}
+
+	cmd.Env = append(os.Environ(), fmt.Sprintf("PACKAGE_DIR=%s", bpPath))
+	cmd.Dir = root
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+
+	if cached {
+		return fmt.Sprintf("%s-cached.tgz", bpPath), nil
+	}
+
+	return fmt.Sprintf("%s.tgz", bpPath), nil
+}
