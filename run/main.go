3945b892bc9706b4719b5c49fe4cdd64d6870fa1
Joshua Casey <joshuatcasey@gmail.com>
2022-06-20 21:15:56 -0500

    Use draft.Planner from packit directly
    - See https://github.com/paketo-buildpacks/cpython/pull/327 for context

--- run/main.go
+++ run/main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/paketo-buildpacks/packit/v2"
 	"github.com/paketo-buildpacks/packit/v2/cargo"
 	"github.com/paketo-buildpacks/packit/v2/chronos"
-	"github.com/paketo-buildpacks/packit/v2/draft"
 	"github.com/paketo-buildpacks/packit/v2/postal"
 	"github.com/paketo-buildpacks/packit/v2/scribe"
 )
@@ -16,7 +15,6 @@ func main() {
 	packit.Run(
 		icu.Detect(),
 		icu.Build(
-			draft.NewPlanner(),
 			postal.NewService(cargo.NewTransport()),
 			icu.NewICULayerArranger(),
 			chronos.DefaultClock,
