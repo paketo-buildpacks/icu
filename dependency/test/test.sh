01256ba27714fa928f3cb639bc3c567b2a29c291
ZephireNZ <brynley+github@zephire.nz>
2025-11-08 17:33:00 +1300

    Update dependency test

--- dependency/test/test.sh
+++ dependency/test/test.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 shopt -s inherit_errexit
 
 main() {
-  local tarball_path version
+  local tarball_path version os arch
 
   while [ "${#}" != 0 ]; do
     case "${1}" in
@@ -15,6 +15,16 @@ main() {
 
       --tarballPath)
         tarball_path="${2}"
+        shift 2
+        ;;
+
+      --os)
+        os="${2}"
+        shift 2
+        ;;
+
+      --arch)
+        arch="${2}"
         shift 2
         ;;
 
@@ -36,15 +46,23 @@ main() {
   dir="$(dirname "${tarball_path}")"
   artifact="$(basename "${tarball_path}")"
 
+  # When --os and --arch are provided, the --platform arg is passed to docker build and run commands.
+  # This assumes the runner has qemu and buildkit set up, and that the docker daemon and cli experimental features are enabled.
+  docker_platform_arg=""
+  if [[ "${os}" != "" && "${arch}" != "" ]]; then
+    docker_platform_arg="--platform ${os}/${arch}"
+    echo "docker commands will be called with ${docker_platform_arg}"
+  fi
+
   if [[ ${artifact} == *"noble"* ]]; then
     echo "Running noble test..."
-    docker build -t test-noble -f noble.Dockerfile .
-    docker run --rm -v "${dir}:/input" test-noble --tarballPath "/input/${artifact}" --expectedVersion "${version}"
+    docker build -t test-noble -f noble.Dockerfile ${docker_platform_arg} .
+    docker run --rm -v "${dir}:/input" ${docker_platform_arg} test-noble --tarballPath "/input/${artifact}" --expectedVersion "${version}" --os "${os}" --arch "${arch}"
 
   elif [[ ${artifact} == *"jammy"* ]]; then
     echo "Running jammy test..."
-    docker build -t test-jammy -f jammy.Dockerfile .
-    docker run --rm -v "${dir}:/input" test-jammy --tarballPath "/input/${artifact}" --expectedVersion "${version}"
+    docker build -t test-jammy -f jammy.Dockerfile ${docker_platform_arg} .
+    docker run --rm -v "${dir}:/input" ${docker_platform_arg} test-jammy --tarballPath "/input/${artifact}" --expectedVersion "${version}" --os "${os}" --arch "${arch}"
   else
     echo "noble or jammy not found - skipping tests"
   fi
